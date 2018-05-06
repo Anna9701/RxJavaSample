f45e15605b6cfbfd5d92ae44ebb8c9cd89a6240f
Anna Wyrwal <202340@edu.p.lodz.pl>
2018-05-06 14:19:23 +0200

    Sample RXJava project

---
+++ app/src/main/java/com/example/anwyr1/rxjavasample/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.anwyr1.rxjavasample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import static io.reactivex.internal.operators.observable.ObservableBlockingSubscribe.subscribe;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        Observable<String> observable = Observable.fromIterable(getStrings());
+        observable.concatMap(s -> Observable.just(s).delay(1, TimeUnit.SECONDS))
+                .subscribe(textView1::setText, System.err::println, () -> textView2.setText(R.string.RxJavaSuccess));
+    }
+
+    private List<String> getStrings() {
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0 ; i < 10; ++i) {
+            strings.add("Hello world, that's RxJava in use! Generated string nr:  " + i);
+        }
+        return strings;
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
